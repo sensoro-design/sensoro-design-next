03c1e4f9ef7a11f9d99c4b78769ed683c421b752
wangxingkang <wang_xingkang@qq.com>
2022-04-11 00:25:38 +0800

    ✨ feat(Checkbox): 优化样式

--- src/Checkbox/__demo__/disabled.demo.tsx
+++ src/Checkbox/__demo__/disabled.demo.tsx
@@ -2,8 +2,15 @@
  * title: 禁用
  * desc: 禁用复选框。
  */
-import { Checkbox } from '@sensoro-design/react';
+import { Checkbox, Space } from '@sensoro-design/react';
 
 export default () => {
-  return (<Checkbox disabled>disabled Checkbox</Checkbox>)
-}
+  return (
+    <Space>
+      <Checkbox checked disabled>
+        已选禁用
+      </Checkbox>
+      <Checkbox disabled>未选禁用</Checkbox>
+    </Space>
+  );
+};
